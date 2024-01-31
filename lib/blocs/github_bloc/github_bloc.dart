53bf5e76eb5e6896d86e8c54b8f09d7034bf2041
DonWirson <twilliamson.valdes@gmail.com>
2024-01-31 19:13:29 -0300

    Added bloc logic to retrieve all commits

--- lib/blocs/github_bloc/github_bloc.dart
+++ lib/blocs/github_bloc/github_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:git_history/models/commit_history_model.dart';
 
 import '../../models/branch_model.dart';
 import '../../services/github_service.dart';
@@ -10,22 +11,65 @@ part 'github_state.dart';
 class GithubBloc extends Bloc<GithubEvent, GithubState> {
   GithubBloc() : super(GithubInitial()) {
     on<GithubEvent>((event, emit) {});
+    on<GotAllCommits>(getAllCommits);
     on<GotAllBranches>(getAllBranches);
+    on<GotAllBranchCommits>(getAllCommitsByBranch);
+  }
+
+  Future<void> getAllCommits(
+      GotAllCommits event, Emitter<GithubState> emit) async {
+    try {
+      emit(
+        GotAllCommitsInProgress(),
+      );
+      //Only added this delay to show the Loading indicator :D
+      await Future.delayed(const Duration(seconds: 2));
+      final branches = await GithubService().getAllCommits();
+      emit(
+        GotAllCommitsSuccessfully(commitHistoryList: branches),
+      );
+    } catch (e) {
+      emit(
+        GotAllCommitsFailure(exception: e.toString()),
+      );
+    }
   }
 
   Future<void> getAllBranches(
       GotAllBranches event, Emitter<GithubState> emit) async {
     try {
       emit(
-        GotAllBranchesInProgress(),
+        GotAllCommitsByBranchInProgress(),
       );
+      //Only added this delay to show the Loading indicator :D
+      await Future.delayed(const Duration(seconds: 2));
       final branches = await GithubService().getBranchtList();
       emit(
-        GotAllBranchesSuccessfull(branches: branches),
+        GotAllBranchesSuccessfully(branches: branches),
       );
     } catch (e) {
       emit(
         GotAllBranchesFailure(exception: e.toString()),
+      );
+    }
+  }
+
+  Future<void> getAllCommitsByBranch(
+      GotAllBranchCommits event, Emitter<GithubState> emit) async {
+    try {
+      emit(
+        GotAllCommitsByBranchInProgress(),
+      );
+      //Only added this delay to show the Loading indicator :D
+      await Future.delayed(const Duration(seconds: 2));
+      final commitHistory =
+          await GithubService().getCommitBySha(event.branchSha);
+      emit(
+        GotAllCommitsByBranchSuccessfully(commitHistory: commitHistory),
+      );
+    } catch (e) {
+      emit(
+        GotAllCommitsByBranchFailure(exception: e.toString()),
       );
     }
   }
--- lib/blocs/github_bloc/github_state.dart
+++ lib/blocs/github_bloc/github_state.dart
@@ -5,12 +5,30 @@ sealed class GithubState {}
 
 final class GithubInitial extends GithubState {}
 
+final class GotAllCommitsInProgress extends GithubState {}
+
+final class GotAllCommitsSuccessfully extends GithubState {
+  final List<CommitHistory> commitHistoryList;
+
+  GotAllCommitsSuccessfully({
+    required this.commitHistoryList,
+  });
+}
+
+final class GotAllCommitsFailure extends GithubState {
+  final String exception;
+
+  GotAllCommitsFailure({
+    required this.exception,
+  });
+}
+
 final class GotAllBranchesInProgress extends GithubState {}
 
-final class GotAllBranchesSuccessfull extends GithubState {
+final class GotAllBranchesSuccessfully extends GithubState {
   final List<BranchModel> branches;
 
-  GotAllBranchesSuccessfull({
+  GotAllBranchesSuccessfully({
     required this.branches,
   });
 }
@@ -19,6 +37,24 @@ final class GotAllBranchesFailure extends GithubState {
   final String exception;
 
   GotAllBranchesFailure({
+    required this.exception,
+  });
+}
+
+final class GotAllCommitsByBranchInProgress extends GithubState {}
+
+final class GotAllCommitsByBranchSuccessfully extends GithubState {
+  final CommitHistory commitHistory;
+
+  GotAllCommitsByBranchSuccessfully({
+    required this.commitHistory,
+  });
+}
+
+final class GotAllCommitsByBranchFailure extends GithubState {
+  final String exception;
+
+  GotAllCommitsByBranchFailure({
     required this.exception,
   });
 }
