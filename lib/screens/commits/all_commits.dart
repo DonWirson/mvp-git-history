a9435c19b99325c1cefa8828b91c3b75133d50f0
DonWirson <twilliamson.valdes@gmail.com>
2024-02-01 14:18:04 -0300

    Added refresh on pull down gesture

--- lib/screens/commits/all_commits.dart
+++ lib/screens/commits/all_commits.dart
@@ -31,77 +31,84 @@ class _AllCommitsState extends State<AllCommits> {
       },
       builder: (context, state) {
         if (state is GotAllCommitsSuccessfully) {
-          return ListView.separated(
-            itemCount: commitList.length,
-            itemBuilder: (context, index) {
-              final commit = commitList[index];
-              final date = commit.committer.date.toString().split(" ");
-              return ListTile(
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                tileColor: Colors.white,
-                leading: Padding(
-                  padding: padding,
-                  child: Column(
+          return RefreshIndicator(
+            onRefresh: onRefresh,
+            child: ListView.separated(
+              itemCount: commitList.length,
+              itemBuilder: (context, index) {
+                final commit = commitList[index];
+                final date = commit.committer.date.toString().split(" ");
+                return ListTile(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  tileColor: Colors.white,
+                  leading: Padding(
+                    padding: padding,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: padding,
+                          child: Text(
+                            "Commiter:",
+                            style: Theme.of(context).textTheme.labelLarge,
+                          ),
+                        ),
+                        Text(
+                          commit.author.name,
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                  title: Padding(
+                    padding: padding,
+                    child: Text(
+                      commit.message,
+                      style: Theme.of(context).textTheme.bodySmall,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  trailing: Column(
                     children: [
                       Padding(
                         padding: padding,
                         child: Text(
-                          "Commiter:",
-                          style: Theme.of(context).textTheme.labelLarge,
+                          date[0].toString(),
+                          style: Theme.of(context).textTheme.labelMedium,
                         ),
                       ),
-                      Text(
-                        commit.author.name,
-                        style: Theme.of(context).textTheme.labelMedium,
-                      ),
+                      Padding(
+                        padding: padding,
+                        //TODO: Refactor this
+                        child: Text(date[1].substring(0, 5)),
+                      )
                     ],
                   ),
-                ),
-                title: Padding(
-                  padding: padding,
-                  child: Text(
-                    commit.message,
-                    style: Theme.of(context).textTheme.bodySmall,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                trailing: Column(
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const Column(
                   children: [
-                    Padding(
-                      padding: padding,
-                      child: Text(
-                        date[0].toString(),
-                        style: Theme.of(context).textTheme.labelMedium,
+                    SizedBox(
+                      height: 40,
+                      child: Center(
+                        child: Icon(Icons.arrow_downward),
                       ),
-                    ),
-                    Padding(
-                      padding: padding,
-                      //TODO: Refactor this
-                      child: Text(date[1].substring(0, 5)),
                     )
                   ],
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const Column(
-                children: [
-                  SizedBox(
-                    height: 40,
-                    child: Center(
-                      child: Icon(Icons.arrow_downward),
-                    ),
-                  )
-                ],
-              );
-            },
+                );
+              },
+            ),
           );
         }
         return const LoadingIndicator();
       },
     );
+  }
+
+  Future<void> onRefresh() async {
+    BlocProvider.of<GithubBloc>(context).add(GotAllCommits());
   }
 }
